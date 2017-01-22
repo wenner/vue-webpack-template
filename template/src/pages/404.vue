ab04e32b616e3ef8ef57e89d79762083ab4fb605
wenner <wenner.cn@gmail.com>
2017-01-22 21:58:23 +0800

    加入app.vue

---
+++ template/src/pages/404.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="bg">
+      <div style="margin:10% 0 0 10%;color:#fff;font-size:32px;font-weight:bold;">
+        <div style="font-size:64px;">404</div>
+        <div style="font-size:48px">维修中,此路不通!</div>
+        <div style="font-size:14px;font-weight:normal;line-height:200%">
+          您要打开的地址({{$route.redirectedFrom}})无法访问,请确定是否通过正常方式进入该页面! <br>
+          <a style="color:#fff;cursor:pointer" @click="goBack"><i class="fa fa-arrow-circle-left"></i> 返回</a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: '',
+    data(){
+      return {}
+    },
+    methods: {
+      goBack(){
+        console.log(this.$route)
+        window.history.go(-1);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  .bg {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    background: url(/static/images/404.jpg) right bottom;
+    background-size: cover;
+  }
+
+  .bg > div > div {
+    margin: 6px 0;
+  }
+</style>
---
+++ template/src/pages/app.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+<template>
+  <div id="app">
+    <template v-if="$auth.ready() && loaded">
+      <content-only v-if="layout.type=='content-only'"></content-only>
+      <horizontal-navigation v-if="layout.type=='horizontal-navigation'"></horizontal-navigation>
+      <vertical-navigation v-if="layout.type=='vertical-navigation'"></vertical-navigation>
+    </template>
+    <template v-else>
+      <div class="loader-backdrop">
+        <!-- Loader -->
+        <div class="loader">
+          <div class="bounce-1"></div>
+          <div class="bounce-2"></div>
+        </div>
+        <!-- /loader -->
+      </div>
+    </template>
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex'
+
+  import ContentOnly from '../components/core/layout/content-only'
+  import HorizontalNavigation from '../components/core/layout/horizontal-navigation'
+  import VerticalNavigation from '../components/core/layout/vertical-navigation'
+
+  export default {
+    name: 'app',
+    computed: mapGetters({
+      layout: 'layout'
+    }),
+    data(){
+      return {
+        loaded: true
+      }
+    },
+    components: {
+      ContentOnly,
+      HorizontalNavigation,
+      VerticalNavigation
+    },
+    ready() {
+      setTimeout(() => {this.loaded = true}, 200);
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" src="assets/css/app.scss"></style>
+
+
+
