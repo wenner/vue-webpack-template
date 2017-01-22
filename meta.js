4c9a946eeed520842a09fb31919956f1487b4e8c
wenner <wenner.cn@gmail.com>
2017-01-22 22:29:11 +0800

    Metalsmith是使用{{}},所以需要去掉.vue

--- meta.js
+++ meta.js
@@ -104,5 +104,6 @@ module.exports = {
     "test/unit/**/*": "unit",
     "test/e2e/**/*": "e2e"
   },
+  "skipInterpolation" : ["*.vue"] , 
   "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
