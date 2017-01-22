ef0e3d4bfd301be69fc1e7530f48eeae2a47649f
wenner <wenner.cn@gmail.com>
2017-01-22 22:40:27 +0800

    测试

--- meta.js
+++ meta.js
@@ -104,6 +104,6 @@ module.exports = {
     "test/unit/**/*": "unit",
     "test/e2e/**/*": "e2e"
   },
-  "skipInterpolation" : ["*.vue"] , 
+  "skipInterpolation" : ["**/*.vue"] , 
   "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
